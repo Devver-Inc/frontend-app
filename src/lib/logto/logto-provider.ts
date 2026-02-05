59a3eb5fede3f7ea823b2aadfe28280852729145
Alex <alexmonac13@gmail.com>
2026-02-05 13:14:48 +0100

    chore: minor changes

--- src/lib/logto/logto-provider.ts
+++ src/lib/logto/logto-provider.ts
@@ -1,7 +1,13 @@
+import { UserScope } from '@logto/react'
 import type { LogtoConfig } from '@logto/react'
 
 export const config: LogtoConfig = {
   endpoint: import.meta.env.VITE_LOGTO_ENDPOINT,
   appId: import.meta.env.VITE_LOGTO_APP_ID,
-  scopes: ['urn:logto:scope:organizations', 'profile', 'email'],
+  scopes: [
+    'access:api',
+    UserScope.Organizations,
+    UserScope.Email,
+    UserScope.Profile,
+  ],
 }
